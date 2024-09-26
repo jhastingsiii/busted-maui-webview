5bf819e037d270973d23d64a3b4f78d1c19a4f13
James Hastings <james.hastings@lifewaymobility.com>
2024-09-26 16:29:54 -0500

    Initial commit

---
+++ MauiApp1/MainPage.xaml.cs
@@ -0,0 +1,19 @@
+﻿namespace MauiApp1
+{
+    public partial class MainPage : ContentPage
+    {
+        int count = 0;
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private async void OnBtnClicked(object sender, EventArgs e)
+        {
+            System.Diagnostics.Debug.WriteLine("Test");
+            await Shell.Current.GoToAsync("WebViewPage");
+        }
+    }
+
+}
